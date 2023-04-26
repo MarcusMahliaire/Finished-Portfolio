bf505e8979c79d11aee90829f11d2fa0f17aabdb
MarcusMahliaire <102937325+MarcusMahliaire@users.noreply.github.com>
2023-04-26 16:46:40 -0700

    Fixed reflection on keyboard

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -32,7 +32,7 @@ export default function Experience() {
             width={2.5}
             height={1.65}
             intensity={65}
-            color={"#ff6900"}
+            color={"#333"}
             rotation={[-0.1, Math.PI, 0]}
             position={[0, 0.55, -1.15]}
           />
